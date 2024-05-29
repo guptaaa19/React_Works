8be1cb9e2c2eb0bc958419ceddc253f1676a1f37
guptaaa19 <118473640+guptaaa19@users.noreply.github.com>
2024-05-29 20:42:25 +0530

    Update nesting_tags.js

--- nesting_tags.js
+++ nesting_tags.js
@@ -25,3 +25,36 @@ console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
+
+
+/* <div id: "parent">
+    <div id: "child">
+        <h1></h1>
+        <h2></h2>   (sibling)
+    </div>
+    <div id: "child2">
+        <h1></h1>
+        <h2></h2> 
+    </div>
+</div>
+*/
+
+const parent = React.createElement(
+    "div",
+    { id: "parent"},
+    React.createElement("div", {id: "child"}, [
+        React.createElement("h1", {}, "Hello Im h1 tag!" ), 
+        React.createElement("h2", {}, "Hello Im h2 tag!" )],
+    React.createElement("div", {id: "child2"}, [
+        React.createElement("h1", {}, "Hello Im h1 tag!" ), 
+        React.createElement("h2", {}, "Hello Im h2 tag!" ),
+    ]),
+    ));
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+// we can create React using both js and jsx but in js the nesting becomes quite lengthy process
+// hence we ue jsx which makes nesting easy and code more readable
