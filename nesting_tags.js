43c77b14d15630b4cb29145e9e7b94d51589b8f8
guptaaa19 <118473640+guptaaa19@users.noreply.github.com>
2024-05-29 19:56:28 +0530

    Update nesting_tags.js

--- nesting_tags.js
+++ nesting_tags.js
@@ -1,16 +1,24 @@
-// creating nesting:
+// create nesting:
 /* <div>
     <div>
         <h1></h1>
+        <h2></h2>   (sibling)
     </div>
-</div> */
+</div>
+if a div has multiple children then you can pass it as array, as shown in the code:
+
+ReactElement(Object) = HTML(Browser understands)
+
+ReactElement: ("what type of tag it is", {object}, "children")
+*/
 
 const parent = React.createElement(
     "div",
     { id: "parent"},
     React.createElement("div", {id: "child"}, 
-    React.createElement("h1", {}, "Hello Im h1 tag!" ))
-);
+    [React.createElement("h1", {}, "Hello Im h1 tag!" ), 
+    React.createElement("h2", {}, "Hello Im h2 tag!" )] //passed as an array(siblings)
+    ));
 
 console.log(parent);
 
